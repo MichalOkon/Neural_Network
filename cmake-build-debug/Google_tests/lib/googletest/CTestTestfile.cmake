a04a9d25a91e75d1d3d1408dad5ea266c58f37ca
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2022-01-21 17:35:18 +0100

    add google test to project

---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jorge/repos/Neural_Network/Google_tests/lib/googletest
+# Build directory: /home/jorge/repos/Neural_Network/cmake-build-debug/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

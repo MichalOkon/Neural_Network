cfa41214e528caba641d16df7d52228ca99a689a
Jorge Romeu <40536127+Jorgeromeu@users.noreply.github.com>
2022-01-21 17:39:46 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jorge/repos/Neural_Network/Google_tests/lib
-# Build directory: /home/jorge/repos/Neural_Network/cmake-build-debug/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
